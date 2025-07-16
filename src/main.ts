73df208f0ba0151c66bd7d01c852421254b34cd1
Cesar Soares Stenico <cesarsst@hotmail.com>
2025-07-16 22:49:01 +0200

    feat: implement global exception filter for improved error handling

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    // Pipe de validação global
     app.useGlobalPipes(new ValidationPipe());
+
+    // Filtro global de exceções
+    app.useGlobalFilters(new AllExceptionsFilter());
 
     await app.listen(process.env.PORT ?? 3000);
 }
